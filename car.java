a7739c34abd62e78e2fe9af015f7b68c3d00b188
Furki <furyenis@gmail.com>
2018-12-24 16:57:45 +0300

    first

---
+++ car.java
@@ -0,0 +1,29 @@
+
+public class car {
+	String colour;
+	double speed;
+	
+	public car (String c, double s) {
+		c=colour;
+		s=speed;
+		
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+
+}
